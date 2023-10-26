249dd8a37a413e69149d4df0414aa701a49b5490
Millianen <www.milli.matti@gmail.com>
2023-10-25 21:10:17 -0300

    Português

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -17,18 +19,27 @@ class AuthController extends Controller
 
     function login(Request $request)
     {
+        //pegar o usuario e senha
         $credentials = $request->validate([
             'email' => 'string|required|email',
             'password' => 'string|required'
         ]);
-
-        if (auth()->attempt($credentials)) {
+        if ($validator->fails()){
+            return response()->json([
+                'message' => 'Dados de entrada inválidos',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+        //verificar se ele existe no banco de dados
+        //Auth::attempt($credentials)
+        if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = Auth::user();
-            $user['token'] = $user->createToken('Passport App')->accesssToken;
+            $user['token'] = $user->createToken('Passaport App')->accessToken;
             return response()->json([
                 'user' => $user
             ], 200);
         }
+        // em caso de erro
         return response()->json([
             'message' => 'Credenciais estão invalidas'
         ], 402);
@@ -38,24 +49,30 @@ class AuthController extends Controller
     {
         Auth::user()->tokens()->delete();
         return response()->json([
-            'message' => 'Logout realizado com sucesso'
+            'message' => 'Logout realizado com sucesso!',
         ]);
     }
+
     function register(Request $request)
     {
-        $request->validate([
-            'name' => ' required|string|max:255',
+        $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
+        if ($validator->fails()){
+            return response()->json([
+                'message'=> 'Error de validação',
+                'errors' => $validator->errors()
+            ], 422);
+        };
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
         return response()->json([
-            'Usuario criado com sucesso',
+            'message' => 'Usuario criado com sucesso',
             'user' => $user
-        ]);
+        ], 201);
     }
 }
